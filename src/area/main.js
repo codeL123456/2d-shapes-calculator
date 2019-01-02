0805bff578028807cb2978f375082043615553c2
youssef alaa <yalaa129@gmail.com>
2019-01-03 00:52:38 +0300

    added the area of square

---
+++ src/area/main.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    $("#calSquare").click(function () {
+        //assigning the value(s) to (a) variable(s)
+        var x = $("#sq-side").val();
+
+        //assiging the result to a variable
+        var result= Math.pow(x, 2);
+
+        //resitting the value(s)
+        $("#sq-side").val("");
+
+        //sitting the css
+        $("#res").css("background-color", "yellow");
+        $("#form").css("background-color", "lightgreen");
+
+        //displaying the values
+        return $("#res").html(result);
+    })
+}
