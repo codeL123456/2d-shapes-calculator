1572f4f67f516f7e7f286bf40ec135ed2ebe15fa
youssef alaa <yalaa129@gmail.com>
2019-01-03 11:47:37 +0300

    added the rectange functionaltiy and some functions

--- src/area/main.js
+++ src/area/main.js
@@ -1,4 +1,33 @@
 window.onload = function () {
+    function resultEditorD(x, y, formulaArea, resultArea) {
+        if (x === "" && y === "") {
+            alert("please provide a number!");
+            document.getElementById(formulaArea). innerHTML = "";
+        } else if (x === "" && y !== "") {
+            document.getElementById(formulaArea). innerHTML = "0 / " + y + " =";
+        } else if (x !== "" && y === "") {
+            document.getElementById(formulaArea). innerHTML = x + " / 0" + " =";
+        } else {
+            document.getElementById(formulaArea).innerHTML = x + " / " + y + " = ";
+        }
+        document.getElementById(formulaArea).style.backgroundColor = "lightgreen";
+        document.getElementById(resultArea).style.backgroundColor = "yellow";
+    }
+    function resultEditorM(x, y, formula, resultArea) {
+        if (x === "" && y === "") {
+            alert("please provide a number!");
+            document.getElementById(formula). innerHTML = "";
+        } else if (x === "" && y !== "") {
+            document.getElementById(formula). innerHTML = "0 x " + y + " =";
+        } else if (x !== "" && y === "") {
+            document.getElementById(formula). innerHTML = x + " x 0" + " =";
+        } else {
+            document.getElementById(formula).innerHTML = x + " x " + y + " = ";
+        }
+        document.getElementById(formula).style.backgroundColor = "lightgreen";
+        document.getElementById(resultArea).style.backgroundColor = "yellow";
+
+    }  
     $("#calSquare").click(function () {
         //assigning the value(s) to (a) variable(s)
         var x = $("#sq-side").val();
@@ -19,5 +48,17 @@ window.onload = function () {
         //displaying the values
         return $("#res").html(result);
     });
+    $("#calRect").click(function () {
+        var x = $("#rec-length");      
+        var y = $("#rec-width");
+        
+        var result = x * y;
 
+        $("#rec-length").val("");
+        $("#rec-width").val("");
+
+        resultEditorM(x, y, "form1", "res1");
+
+        return $("#res1").html(result);
+    });
 }
