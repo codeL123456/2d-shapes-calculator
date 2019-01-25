0f9079e75d492e1f86c5beb5097710ff36e5d750
youssef alaa <yalaa129@gmail.com>
2019-01-25 09:27:02 +0300

    compleated the retangle

--- src/peremeter/main.js
+++ src/peremeter/main.js
@@ -7,9 +7,32 @@ window.onload = function () {
              } else {
                 document.getElementById(equationArea).innerHTML = x + " x 4 = ";
             }
+            document.getElementById(equationArea).style.backgroundColor = "lightgreen";
+            document.getElementById(resultArea).style.backgroundColor = "yellow";
+        },
+        multiplyBy2AndAddcss(x, y, equationArea, resultArea){
+            if (x === "" && y === "") {
+                alert("please provide a number!");
+                document.getElementById(equationArea).innerHTML = "";
+            } else if (x === "" && y !== "") {
+                document.getElementById(equationArea). innerHTML = "2(0 + " + y + ") = ";
+            } else if (x !== "" && y === "") {
+                document.getElementById(equationArea). innerHTML = "2( " + x + " + 0) = ";
+            } else {
+                document.getElementById(equationArea).innerHTML = "2( " + x + " + " + y + ") =";
+            }
 
             document.getElementById(equationArea).style.backgroundColor = "lightgreen";
             document.getElementById(resultArea).style.backgroundColor = "yellow";
+        },
+        multiplyBy2AndAdd(x, y, equationArea, resultArea){
+            let id = document.getElementById(x).value;
+            let id1 = document.getElementById(y).value;
+            let result = 2 * (+id + +id1);
+            document.getElementById(x).value = "";
+            document.getElementById(y).value = "";
+            this.multiplyBy2AndAddcss(id, id1, equationArea, resultArea);
+            return document.getElementById(resultArea).innerHTML = result;
         },
         multiplyBy4(x, equationArea, resultArea){
             let id  = document.getElementById(x).value;
@@ -22,4 +45,7 @@ window.onload = function () {
     $("#calSquare").click(function () {
         calculator.multiplyBy4("sq-side", "form", "res");
     });
+    $("#calRect").click(function () {
+        calculator.multiplyBy2AndAdd("rec-length", "rec-width", "form1", "res1");
+    })
 }
